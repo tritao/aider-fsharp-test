65b2d22a7e9dbcd754e77d80d917c19adf1720c8
Joao Matos <joao@tritao.eu>
2024-10-06 23:25:52 +0100

    refactor: Simplify and reorganize parsing functions in Parser.fs

--- Parser.fs
+++ Parser.fs
@@ -17,49 +17,46 @@ type Function =
       Parameters: (string * string) list
       Body: Statement list }
 
-let parseStatement (tokens: Token list) : Statement * Token list =
-    // Implementation of parseStatement
-    // (This should be the actual implementation from the Parser module)
-    failwith "Not implemented"
+let rec parseExpression tokens =
+    match tokens with
+    | Lexer.Number n :: rest -> Number n, rest
+    | Lexer.Identifier v :: rest -> Variable v, rest
+    | _ -> failwith "Unexpected token in expression"
+
+let rec parseStatement tokens =
+    match tokens with
+    | Keyword "return" :: rest ->
+        let expr, rest' = parseExpression rest
+        Return expr, rest'
+    | Keyword "if" :: OpenParen :: rest ->
+        let expr, rest' = parseExpression rest
+        match rest' with
+        | CloseParen :: OpenBrace :: rest'' ->
+            let stmt, rest''' = parseStatement rest''
+            If (expr, stmt), rest'''
+        | _ -> failwith "Expected closing parenthesis and opening brace"
+    | OpenBrace :: rest ->
+        let rec parseBlock tokens acc =
+            match tokens with
+            | CloseBrace :: rest' -> Block (List.rev acc), rest'
+            | _ ->
+                let stmt, rest' = parseStatement tokens
+                parseBlock rest' (stmt :: acc)
+        parseBlock rest []
+    | _ -> failwith "Unexpected token in statement"
+
+let rec parseParameters tokens acc =
+    match tokens with
+    | Identifier t :: Identifier n :: rest ->
+        let param = (t, n)
+        match rest with
+        | Lexer.Comma :: rest' -> parseParameters rest' (param :: acc)
+        | Lexer.CloseParen :: rest' -> List.rev (param :: acc), rest'
+        | _ -> failwith "Unexpected token in parameters"
+    | _ -> failwith "Unexpected token in parameters"
 
 let parse (tokens: Token list) : Function =
-    let rec parseExpression tokens =
-        match tokens with
-        | Lexer.Number n :: rest -> Number n, rest
-        | Lexer.Identifier v :: rest -> Variable v, rest
-        | _ -> failwith "Unexpected token in expression"
 
-    let rec parseStatement tokens =
-        match tokens with
-        | Keyword "return" :: rest ->
-            let expr, rest' = parseExpression rest
-            Return expr, rest'
-        | Keyword "if" :: OpenParen :: rest ->
-            let expr, rest' = parseExpression rest
-            match rest' with
-            | CloseParen :: OpenBrace :: rest'' ->
-                let stmt, rest''' = parseStatement rest''
-                If (expr, stmt), rest'''
-            | _ -> failwith "Expected closing parenthesis and opening brace"
-        | OpenBrace :: rest ->
-            let rec parseBlock tokens acc =
-                match tokens with
-                | CloseBrace :: rest' -> Block (List.rev acc), rest'
-                | _ ->
-                    let stmt, rest' = parseStatement tokens
-                    parseBlock rest' (stmt :: acc)
-            parseBlock rest []
-        | _ -> failwith "Unexpected token in statement"
-
-    let rec parseParameters tokens acc =
-        match tokens with
-        | Identifier t :: Identifier n :: rest ->
-            let param = (t, n)
-            match rest with
-            | Lexer.Comma :: rest' -> parseParameters rest' (param :: acc)
-            | Lexer.CloseParen :: rest' -> List.rev (param :: acc), rest'
-            | _ -> failwith "Unexpected token in parameters"
-        | _ -> failwith "Unexpected token in parameters"
 
     match tokens with
     | Keyword "public" :: Keyword "static" :: Keyword "int" :: Identifier name :: OpenParen :: rest ->
